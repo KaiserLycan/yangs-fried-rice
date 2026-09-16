a574e1c3f75a1325f8515f30e57feb9879563570
KaiserLycan <josephreyramirezpanti+github@gmail.com>
2026-09-16 20:22:44 +0800

    (fix): removed duplicated get_customer_order_history.

--- types/database.types.ts
+++ types/database.types.ts
@@ -709,10 +709,6 @@ export type Database = {
       [_ in never]: never
     }
     Functions: {
-      get_customer_order_history: {
-        Args: { p_customer_id: string }
-        Returns: Json
-      }
 
       cancel_customer_order: {
         Args: {
